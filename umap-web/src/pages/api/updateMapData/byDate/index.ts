5e802793f104ba9fd9584586aac1b4d669f9199e
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-06-15 17:04:10 +0700

    Finishes api for updateOSMPostGis with version date control

---
+++ umap-web/src/pages/api/updateMapData/byDate/index.ts
@@ -0,0 +1,149 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { spawn } from 'child_process'
+import { urlForAxiosHCMOsm, urlForTrackingHCMOsmDate } from '../../data/updateRelatedUrl';
+import axios from 'axios';
+import fs from 'fs'
+import updatedDates from '../../data/updatedDates';
+import { getParentDirectory } from '../getDates';
+
+async function fetchDataFromTPHCMOsm(parentDirectory: string) {
+    const response = await axios.get(urlForAxiosHCMOsm);
+    const data = await response.data
+    fs.writeFile(parentDirectory + '\\src\\pages\\api\\data\\map.osm', data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    }
+    );
+}
+
+async function updateToPostgis(parentDirectory: string) {
+    const childProcess = spawn('osm2pgsql',
+        ['-a', '--slim', '-d', process.env.dbname!, '-P', process.env.DBport!, '-U', process.env.user!, '--password', '-H',
+            process.env.hostname!, '--extra-attributes', '-S', parentDirectory + "\\src\\pages\\api\\data\\default.style", parentDirectory + "\\src\\pages\\api\\data\\map.osm"]);
+
+    // Handle stdout data
+    childProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+
+    });
+
+    // Handle stderr data
+    childProcess.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    // Provide password input
+    childProcess.stdin.write('trivandeptrai\n');
+
+    // Handle process exit
+    childProcess.on('exit', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
+}
+
+interface DateCustomRequest extends NextApiRequest {
+    query: {
+      dateMode?: "byHour"|"byDay";
+    };
+  }
+
+export default async function byDate(req: DateCustomRequest, res: NextApiResponse) {
+    // get dateMode from req.query
+    const dateMode = req.query.dateMode
+    // if dateMode is not byDay or byHour, return error
+    if (dateMode!=="byDay"&&dateMode!=="byHour") {
+        res.status(400).json({
+            state:"failed",
+            message:"dateMode must be byDay or byHour"
+        })
+        return
+    }
+    let parentDirectory = getParentDirectory()
+
+    // start checking hcm osm update date 
+    const response = await axios.get(urlForTrackingHCMOsmDate);
+    const data = await response.data
+
+    // find "abbr" element in this response.data string
+    const abbrIndex = data.indexOf("abbr");
+    // get the title of abbr element
+    const title = data.substring(abbrIndex + 12, data.indexOf(">", abbrIndex) - 1);
+    // title now is Date String, turn it to date
+    const date = new Date(title);
+    // turn dates in updatedDates to date
+    let lastOsmMapUpdatedDate = new Date(updatedDates.lastOsmMapUpdatedDate)
+    let lastAppMapUpdatedDate = new Date(updatedDates.lastAppMapUpdatedDate)
+
+    
+    // instantly update the lastOsmMapUpdatedDate to file updatedDates.ts
+    // @ts-ignore
+    if (date > lastAppMapUpdatedDate) {
+        if(date > lastOsmMapUpdatedDate)
+        // write new date to ../../data/updatedDates.ts
+        fs.writeFileSync(parentDirectory + "\\src\\pages\\api\\data\\updatedDates.ts", `const updatedDates = {
+    lastAppMapUpdatedDate: "${lastAppMapUpdatedDate.toISOString()}",
+    lastOsmMapUpdatedDate: "${date.toISOString()}"
+        }
+        
+export default updatedDates;`, 
+        // @ts-ignore
+        (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+            }
+        );
+        // calculate the difference in days and hours
+        // @ts-ignore
+        const diff = date - lastAppMapUpdatedDate;
+        // check if diff is larger than 1 day
+        if ((dateMode==="byDay"&&diff >= 1000 * 60 * 60 * 24)||(dateMode==="byHour"&&diff >= 1000 * 60 * 60)) {
+            // update the map
+            console.log("Fetching new HCM city osm\n")
+            await fetchDataFromTPHCMOsm(parentDirectory)
+            console.log("Finished fetching new data, start updating to postgis\n")
+            await updateToPostgis(parentDirectory)
+            console.log("Finished updating to postgis\n")
+            // update lastAppMapUpdatedDate
+            fs.writeFileSync(parentDirectory + "\\src\\pages\\api\\data\\updatedDates.ts", `const updatedDates = {
+    lastAppMapUpdatedDate: "${date.toISOString()}",
+    lastOsmMapUpdatedDate: "${date.toISOString()}"
+        }
+        
+export default updatedDates;`, 
+            // @ts-ignore
+            (err) => {
+                if (err) throw err;
+                console.log('Data written to file');
+                }
+            );
+            
+            // response
+            res.status(200).json({
+                state:"success",
+                lastAppMapUpdatedDate: lastOsmMapUpdatedDate,
+                lastOsmMapUpdatedDate: lastOsmMapUpdatedDate,
+                message:"Updated HCM city map successfully"
+            })
+        }
+        else
+        {
+            // response
+            res.status(200).json({
+                state:"failed",
+                lastAppMapUpdatedDate: lastAppMapUpdatedDate,
+                lastOsmMapUpdatedDate: lastOsmMapUpdatedDate,
+                message:"Time difference less than 1 " + (dateMode==="byDay"?"day":dateMode==="byHour"?"hour":"") + " so no need to update HCM city map"
+            })
+        }
+    }
+    else
+    {
+        // response
+        res.status(200).json({
+            state:"failed",
+            lastAppMapUpdatedDate: lastAppMapUpdatedDate,
+            lastOsmMapUpdatedDate: lastOsmMapUpdatedDate,
+            message:"HCM city map is up to date"
+        })
+    }
+}
---
+++ umap-web/src/pages/api/updateMapData/getDates/index.ts
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+import updatedDates from '../../data/updatedDates';
+import fs from 'fs'
+import { urlForTrackingHCMOsmDate } from '../../data/updateRelatedUrl';
+import path from 'path';
+
+export function getParentDirectory() {
+    let parentDirectory = path.resolve(__dirname).substring(0, path.resolve(__dirname).lastIndexOf('\\'))
+    parentDirectory = parentDirectory.substring(0, parentDirectory.lastIndexOf('\\'))
+    parentDirectory = parentDirectory.substring(0, parentDirectory.lastIndexOf('\\'))
+    parentDirectory = parentDirectory.substring(0, parentDirectory.lastIndexOf('\\'))
+    parentDirectory = parentDirectory.substring(0, parentDirectory.lastIndexOf('\\'))
+    return parentDirectory
+}
+
+export default async function getDates(req: NextApiRequest, res: NextApiResponse) {
+    const response = await axios.get(urlForTrackingHCMOsmDate);
+    const data = await response.data
+    
+    // find "abbr" element in this response.data string
+    const abbrIndex = data.indexOf("abbr");
+    // get the title of abbr element
+    const title = data.substring(abbrIndex + 12, data.indexOf(">", abbrIndex)-1);
+    // title now is Date String, turn it to date
+    const date = new Date(title);
+    // turn dates in updatedDates to date
+    let lastOsmMapUpdatedDate = new Date(updatedDates.lastOsmMapUpdatedDate)
+    let lastAppMapUpdatedDate = new Date(updatedDates.lastAppMapUpdatedDate)
+
+    // instantly update the lastOsmMapUpdatedDate to file updatedDates.ts
+    let parentDirectory = getParentDirectory()
+    if (date > lastOsmMapUpdatedDate) {
+        // write new date to ../../data/updatedDates.ts
+        fs.writeFileSync(parentDirectory + "\\src\\pages\\api\\data\\updatedDates.ts", `const updatedDates = {
+    lastAppMapUpdatedDate: "${lastAppMapUpdatedDate.toISOString()}",
+    lastOsmMapUpdatedDate: "${date.toISOString()}"
+        }
+        
+export default updatedDates;`, 
+        // @ts-ignore
+        (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+            }
+        );
+    }
+
+    // calculate the difference in days and hours
+    // @ts-ignore
+    const diff = date - lastAppMapUpdatedDate;
+
+    // convert to days
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    // convert to hours after minus days
+    const hours = Math.floor((diff - days * (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    const diffCalculated = (days.toString()+((days===0||days===1)?" day and ":" days and ") + hours.toString() + ((hours===0||hours===1)?" hour":" hours"))
+
+    // return res
+    res.status(200).json({
+        lastAppMapUpdatedDate: lastAppMapUpdatedDate,
+        lastOsmMapUpdatedDate: date,
+        diffCalculated: diffCalculated
+    })
+}
