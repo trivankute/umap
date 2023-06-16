05b8ef27d91dd15736907f06ac633942b8d590f8
lostmymind-18 <supercool1170@gmail.com>
2023-06-16 10:45:03 +0700

    view map

--- umap-web/components/map.tsx
+++ umap-web/components/map.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React ,{ useState} from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
-
+import "./map.css";
 export default function ViewMap(){
     const [center, setCenter] = useState({lat:10.879961,lng:106.810877});
     const ZOOM_LEVEL = 9;
@@ -11,7 +11,7 @@ export default function ViewMap(){
           center ={center}
           zoom={ZOOM_LEVEL}
           scrollWheelZoom={true} 
-          style={{height:"800px",width:"800px"}}
+          style={{height:"100vh",width:"100vw"}}
         >
         <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
