807c347fa9572d254b3eccdb312ef243b29d969c
Minh Nhat <wunnynhatminh2602@gmail.com>
2023-06-16 09:29:08 +0700

    Changed layout

---
+++ umap-web/src/components/DirectionBox.tsx
@@ -0,0 +1,102 @@
+'use client'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDirections, faSearch, faMapMarkerAlt, faCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import './directionBoxStyle.component.css'
+
+interface DirectionBoxProps {
+    onDirectionCancel: () => void;
+  }
+
+interface SearchResult {
+    place_id: string;
+    display_name: string;
+  }
+  
+  let addresses: SearchResult[] = [
+    {
+      place_id: '1',
+      display_name: '147 Nguyen Cong Tru'
+    }, 
+    
+    {
+      place_id: '2',
+      display_name: '69 Tan Lap, Dong Hoa, Di An, Binh Duong'
+    }
+  ]
+
+const DirectionBox: React.FC<DirectionBoxProps> = (props) => {
+        const [listPlace, setListPlace] = useState<SearchResult[]>([]);
+        
+        const handleSearch = () => {
+            setListPlace(addresses)
+        };
+
+        const handleCancel = () => {
+            props.onDirectionCancel()
+        }
+  return (
+    <div className="direction-container">
+        <div className="direction-tool">
+            <button className="direction-button">
+                <FontAwesomeIcon icon={faDirections}/>
+            </button>
+            <button className="cancel-button" onClick={handleCancel}>
+                <FontAwesomeIcon icon={faTimes}/>
+            </button>
+        </div>
+        <div className="direction-box">
+            <div className="direction-start">
+                <div className="icon-start">
+                    <FontAwesomeIcon icon={faCircle} />
+                </div>
+                
+                <input 
+                    type="text"
+                    placeholder='Chọn điểm bắt đầu...'
+                    className='search-input' 
+                />
+                <button className="search-button" onClick={handleSearch}>
+                    <FontAwesomeIcon icon={faSearch}/>
+                </button>
+            </div>
+            <div className="direction-des">
+                <div className="icon-direction">
+                    <FontAwesomeIcon icon={faMapMarkerAlt} />
+                </div>
+                <input 
+                    type="text"
+                    placeholder='Chọn điểm đến...'
+                    className='search-input'
+                />    
+                <button className="search-button" onClick={handleSearch}>
+                    <FontAwesomeIcon icon={faSearch}/>
+                </button>
+            </div>
+        </div>
+        <div className="direction-address">
+            <List className='list-address' component="nav" aria-label="main mailbox folders">
+                {listPlace.map((item) => {
+                return (
+                    <div key={item?.place_id}>
+                    <ListItem
+                        onClick={() => {
+                        }}
+                    >
+                        <ListItemIcon>
+                        <FontAwesomeIcon icon={faMapMarkerAlt} />
+                        </ListItemIcon>
+                        <ListItemText primary={item?.display_name} />
+                    </ListItem>
+                    <Divider />
+                    </div>
+                );
+                })}
+            </List>
+        </div>
+    </div>
+  )
+}
+
+export default DirectionBox
---
+++ umap-web/src/components/SearchBox.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDirections, faClock, faSearch, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material';
+import "./styles.component.css"
+
+interface SearchBoxProps {
+  onSearchDirection: () => void;
+}
+
+interface SearchResult {
+  place_id: string;
+  display_name: string;
+}
+
+let addresses: SearchResult[] = [
+  {
+    place_id: '1',
+    display_name: '147 Nguyen Cong Tru'
+  }, 
+  
+  {
+    place_id: '2',
+    display_name: '69 Tan Lap, Dong Hoa, Di An, Binh Duong'
+  }
+]
+
+const SearchBox: React.FC<SearchBoxProps> = (props) => {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchText, setSearchText] = useState("");
+  const [listPlace, setListPlace] = useState<SearchResult[]>([]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log('Đã tìm kiếm:', searchValue);
+    setListPlace(addresses)
+  };
+
+  const handleDirection = () => {
+    props.onSearchDirection();
+  }
+  return (
+    <div className='container'>
+        <div className="search-container">
+            <label htmlFor="search-input"></label>
+            <input
+                type="text"
+                placeholder="Tìm kiếm"
+                className="search-input"
+                value={searchValue}
+                onChange={handleInputChange}
+            />
+
+            <button className="search-button" onClick={handleSearch}>
+                <FontAwesomeIcon icon={faSearch} className='faSearch'/>
+            </button>
+            <button className="search-direction" onClick={handleDirection}>
+                <FontAwesomeIcon icon={faDirections} className='faDirections'/>
+            </button>
+        </div>
+
+        <div className='search-box'>
+          <List className='list-address' component="nav" aria-label="main mailbox folders">
+            {listPlace.map((item) => {
+              return (
+                <div key={item?.place_id}>
+                  <ListItem
+                    onClick={() => {
+                    }}
+                  >
+                    <ListItemIcon>
+                      <FontAwesomeIcon icon={faMapMarkerAlt} />
+                    </ListItemIcon>
+                    <ListItemText primary={item?.display_name} />
+                  </ListItem>
+                  <Divider />
+                </div>
+              );
+            })}
+          </List>
+      </div>
+    </div>
+  );
+}
+
+export default SearchBox
