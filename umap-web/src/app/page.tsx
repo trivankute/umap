9c37b2787ba5c514243694198cda3d24a28483f6
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-06-18 20:48:01 +0700

    hoan thien css va add motion to components

--- umap-web/src/app/page.tsx
+++ umap-web/src/app/page.tsx
@@ -1,32 +1,38 @@
 'use client'
-import React,{useState} from "react";
+import React, { useState } from "react";
 import SearchBox from "@/components/MapTools/SearchBox/SearchBox"
 import DirectionBox from "@/components/MapTools/DirectionBox/DirectionBox"
 import './styles.component.css'
 import ViewMap from "@/components/Map/Map";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Home() {
-  const [showDirectionBox, setShowDirectionBox] = useState(false);
+  const [showDirectionBox, setShowDirectionBox] = useState(true);
 
   const handleSearchDirection = () => {
     setShowDirectionBox(true);
   };
-  
+
   const handleSearchCancel = () => {
     setShowDirectionBox(false);
   };
-  
+
   return (
     <div className="relative">
-      <div className="absolute" style={{zIndex:10000}}>
-        {showDirectionBox ? (
-          <DirectionBox onDirectionCancel={handleSearchCancel}/>
-        ) : (
-          <SearchBox onSearchDirection={handleSearchDirection} />
-        )}
+      <div className="absolute" style={{ zIndex: 10000 }}>
+        <AnimatePresence mode='wait'>
+          {showDirectionBox &&
+            <DirectionBox onDirectionCancel={handleSearchCancel} />
+          }
+        </AnimatePresence>
+        <AnimatePresence mode='wait'>
+          {!showDirectionBox &&
+            <SearchBox onSearchDirection={handleSearchDirection} />
+          }
+        </AnimatePresence>
       </div>
       <div className="relative">
-        <ViewMap/>
+        <ViewMap />
       </div>
     </div>
   )
--- umap-web/src/components/MapTools/DirectionBox/DirectionBox.tsx
+++ umap-web/src/components/MapTools/DirectionBox/DirectionBox.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDirections, faSearch, faMapMarkerAlt, faCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './directionBoxStyle.component.css'
 import AddressList from '../AddressList/AddressList';
+import { motion } from 'framer-motion';
 
 interface DirectionBoxProps {
     onDirectionCancel: () => void;
@@ -22,7 +23,7 @@ interface SearchResult {
     
     {
       place_id: '2',
-      display_name: '69 Tan Lap, Dong Hoa, Di An, Binh Duong'
+      display_name: '69 Tan Lap, Dong Hoa, Di An, Binh Duong ssssssssssssss'
     }
   ]
 
@@ -37,48 +38,54 @@ const DirectionBox: React.FC<DirectionBoxProps> = (props) => {
             props.onDirectionCancel()
         }
   return (
-    <div className="direction-container shadow-xl max-w-[300px] md:max-w-[500px]">
+    <motion.div 
+        initial={{opacity:0, x: -500}}
+        animate={{opacity:1, x: 0}}
+        exit={{opacity:0, x: -500}}
+        transition={{duration: 0.5}}
+        key="direction-box"
+        className="direction-container shadow-xl max-w-[300px] md:max-w-[400px]">
         <div className="direction-tool flex items-center justify-between p-2 border-b-2">
             <button className="direction-button w-[40px] d-flex justify-center items-center rounded-md group hover:bg-gray-100">
                 <FontAwesomeIcon icon={faDirections} className="group-hover:text-green-400"/>
             </button>
-            <button className="cancel-button w-[40px] d-flex justify-center items-center rounded-md group hover:bg-gray-100" onClick={handleCancel}>
-                <FontAwesomeIcon icon={faTimes} className="group-hover:text-slate-400"/>
+            <button className="cancel-button w-[40px] flex justify-center items-center rounded-md group hover:bg-gray-100" onClick={handleCancel}>
+                <FontAwesomeIcon icon={faTimes} className="group-hover:text-slate-800"/>
             </button>
         </div>
-        <div className="direction-box p-2">
+        <div className="direction-box p-2 w-full">
             <div className="direction-start">
-                <div className="icon-start flex justify-center items-center">
+                <div className="icon-start w-[40px] flex justify-center items-center">
                     <FontAwesomeIcon icon={faCircle} />
                 </div>
                 
                 <input 
                     type="text"
                     placeholder='Chọn điểm bắt đầu...'
-                    className='search-input outline-none focus:ring-1 focus:ring-green-400 focus:border-transparent text-sm md:text-base' 
+                    className='search-input w-full outline-none focus:ring-1 focus:ring-green-400 focus:border-transparent text-sm md:text-base' 
                 />
-                <button className="group search-button w-[40px] flex justify-center items-center hover:bg-green-200 hover:border-transparent" onClick={handleSearch}>
+                <button className="group search-button w-[40px] flex justify-center items-center hover:bg-green-400 hover:border-transparent" onClick={handleSearch}>
                     <FontAwesomeIcon icon={faSearch} className="group-hover:text-white"/>
                 </button>
             </div>
             <div className="direction-des">
-                <div className="icon-direction flex justify-center items-center">
+                <div className="icon-direction w-[40px] flex justify-center items-center">
                     <FontAwesomeIcon icon={faMapMarkerAlt} />
                 </div>
                 <input 
                     type="text"
                     placeholder='Chọn điểm đến...'
-                    className='search-input outline-none focus:ring-1 focus:ring-green-400 focus:border-transparent text-sm md:text-base'
+                    className='search-input w-full outline-none focus:ring-1 focus:ring-green-400 focus:border-transparent text-sm md:text-base'
                 />    
-                <button className="group search-button w-[40px] flex justify-center items-center hover:bg-green-200 hover:border-transparent" onClick={handleSearch}>
+                <button className="group search-button w-[40px] flex justify-center items-center hover:bg-green-400 hover:border-transparent" onClick={handleSearch}>
                     <FontAwesomeIcon icon={faSearch} className="group-hover:text-white"/>
                 </button>
             </div>
         </div>
-        <div className="direction-address">
+        <div className="direction-address  max-w-[300px] md:max-w-[500px]">
             <AddressList listPlace={listPlace}/>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
