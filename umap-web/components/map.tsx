585d09323e60d656719d01d3b9bb5ffbb5744b1b
lostmymind-18 <supercool1170@gmail.com>
2023-06-20 06:05:04 +0700

    add marker & base layers

--- umap-web/components/map.tsx
+++ umap-web/components/map.tsx
@@ -1,10 +1,48 @@
 "use client";
 import React ,{ useState} from "react";
-import { MapContainer, TileLayer , ZoomControl} from "react-leaflet";
+import L, { popup } from "leaflet";
+import { MapContainer, Marker, TileLayer , ZoomControl, useMapEvents,Popup,useMap} from "react-leaflet";
+import { WMSTileLayer, LayersControl} from 'react-leaflet';
+const { BaseLayer, Overlay } = LayersControl;
 import './map.css';
-export default function ViewMap(){
+
+
+function LocationMarkers() {
+  const [markers, setMarkers] = useState([]);
+  const map = useMapEvents({
+    click(e) {
+      setMarkers(markers => markers.concat([e.latlng]));
+      console.log(markers);
+    }
+  });
+
+  const removeMarker = (index) => {
+    const newMarkers = markers.filter((_, i) => i !== index);
+    setMarkers(newMarkers);
+  };
+
+
+  return (
+    <>
+      {markers.map((marker,idx) => 
+      <Marker 
+        key = {idx} 
+        position={marker} 
+        eventHandlers={
+          {
+            click(){
+              removeMarker(idx);
+            }
+          }
+        }
+      />)}
+    </>
+  );
+}
+
+export default function MapView(){
     const [center, setCenter] = useState({lat:10.879961,lng:106.810877});
-    const ZOOM_LEVEL = 9;
+    const ZOOM_LEVEL = 12;
     return (
         <>
         <MapContainer
@@ -14,11 +52,19 @@ export default function ViewMap(){
           zoomControl = {false} 
           style={{height:"100vh",width:"100vw"}}
         >
-        <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+      
+
+      <LayersControl>
+        <BaseLayer checked name="U-MAP">
+          <WMSTileLayer url="https://umap.dientoan.vn/geoserver/ows?" layers='TVtesting:planet_osm_line'/>
+        </BaseLayer>
+        <BaseLayer checked name="OSM">
+          <WMSTileLayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"/>
+        </BaseLayer>
+      </LayersControl>
+
         <ZoomControl position="topright" />
+        <LocationMarkers/>
         </MapContainer>
         </>
       );
--- umap-web/src/app/page.tsx
+++ umap-web/src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
-import ViewMap from '../../components/map'
+import MapView from '../../components/map'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <ViewMap/>
+      <MapView/>
     </main>
   )
 }
