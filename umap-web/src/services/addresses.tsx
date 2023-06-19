c4d9250679b9469690efbb893178dfe573bb8ecc
Minh Nhat <wunnynhatminh2602@gmail.com>
2023-06-19 11:10:15 +0700

    changed address.tsx

--- umap-web/src/services/addresses.tsx
+++ umap-web/src/services/addresses.tsx
@@ -1,5 +1,7 @@
 export default async function getAddresses() {
     try {
+        const url = process.env.ADDRESS_URL
+        console.log(url)
         const response = await fetch('http://localhost:3001/address');
         const data = await response.json();
         return data
