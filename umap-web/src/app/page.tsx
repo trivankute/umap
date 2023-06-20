9bec0962520eb886512d6715c730f14a379f36f0
lostmymind-18 <supercool1170@gmail.com>
2023-06-20 09:12:15 +0700

    view map

--- umap-web/src/app/page.tsx
+++ umap-web/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import MapView from '../../components/map'
+import MapView from '../components/Map/map'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
