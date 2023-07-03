2d111f01dc49e654443725360e3524c0e732dd63
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-07-03 17:27:05 +0700

    adjust addressParser data and integrate new addressParser to app

--- umap-web/src/pages/api/index.ts
+++ umap-web/src/pages/api/index.ts
@@ -6,15 +6,36 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // prisma call
     const parentDirectory = getParentDirectory()
     const data = await prisma.$queryRawUnsafe(`
-    WITH
-        specific_district as (
-        select district, district_geometry from districts_forsearch
-        order by levenshtein(unaccent(lower(district)), unaccent(lower($1)))
-        limit 1)
-    select osm_id,name,st_x(st_transform(st_centroid(way),4326)),st_y(st_transform(st_centroid(way),4326)), 
-    st_contains(specific_district.district_geometry, planet_osm_polygon.way) from planet_osm_polygon, specific_district
-    where name = 'Trường Đại học Bách khoa TP.HCM'`, "Quận 10")
-    console.log(data)
+    select name from planet_osm_point
+    where boundary isnull and ( name notnull ) and admin_level isnull
+    `)
+    res.status(200).json(data)
+
+    // const polygons = await prisma.$queryRawUnsafe(`
+    // WITH
+    // specific_district as (
+    // select district, district_geometry from districts_forsearch
+    // order by levenshtein(unaccent(lower(district)), unaccent(lower($1)))
+    // limit 1),
+    // specific_ward as (select ward, ward_geometry from wards_forsearch, specific_district
+    // where st_contains(specific_district.district_geometry, ward_geometry)
+    // order by levenshtein(unaccent(lower(ward)), unaccent(lower($2)))
+    // limit 1)
+    // select osm_id::text,"addr:housenumber", "addr:street",name,st_x(st_transform(st_centroid(way),4326)),st_y(st_transform(st_centroid(way),4326))
+    // from planet_osm_polygon, specific_ward
+    // where (name notnull or "addr:housenumber" notnull or "addr:street" notnull) and st_contains(specific_ward.ward_geometry, planet_osm_polygon.way)`, "Quận 10", 'Phường 14')
+    
+    // const points = await prisma.$queryRawUnsafe(`
+    // WITH
+    //     specific_district as (
+    //     select district, district_geometry from districts_forsearch
+    //     order by levenshtein(unaccent(lower(district)), unaccent(lower($1)))
+    //     limit 1)
+    // select osm_id::text,"addr:housenumber", "addr:street",name,st_x(st_transform(st_centroid(way),4326)),st_y(st_transform(st_centroid(way),4326))
+    // from planet_osm_point, specific_district
+    // where (name notnull or "addr:housenumber" notnull or "addr:street" notnull) and st_contains(specific_district.district_geometry, planet_osm_point.way)`, "Quận 10")
+
+    // name = 'Trường Đại học Bách khoa TP.HCM'`, "Quận 10"
     // write data down
-    // res.status(200).json(data)
+    // res.status(200).json(polygons)
 }
--- umap-web/src/pages/api/map/getAddresses/fromText/index.ts
+++ umap-web/src/pages/api/map/getAddresses/fromText/index.ts
@@ -81,33 +81,33 @@ export default async function handler(req: CustomNextApiRequest, res: NextApiRes
 
         // prisma call
         ////////////////////////////////// old version
-        //     let points = await prisma.$queryRawUnsafe(`WITH
-        // specific_district as (
-        // select district, district_geometry from districts_forsearch
-        // order by levenshtein(unaccent(lower(district)), unaccent(lower($1)))
-        // limit 1),
-        // specific_ward as (select ward, ward_geometry from wards_forsearch, specific_district
-        // where st_contains(specific_district.district_geometry, ward_geometry)
-        // order by levenshtein(unaccent(lower(ward)), unaccent(lower($2)))
-        // limit 1),
-        // specific_roads as (select * from planet_osm_line,specific_ward
-        // where boundary isnull and name notnull and st_contains(specific_ward.ward_geometry, way)),
-        // specific_points as (select * from planet_osm_point,specific_ward
-        // where boundary isnull and (name notnull or "addr:street" notnull or "addr:housenumber" notnull) and st_contains(specific_ward.ward_geometry, way))
-
-        // select osm_id::text, "addr:housenumber", "addr:street", name from specific_points
-        // `, district, ward)
-        /////////////////////////////////////////////////// new version
-        let points = await prisma.$queryRawUnsafe(`WITH
+            let points = await prisma.$queryRawUnsafe(`WITH
         specific_district as (
-            select district, district_geometry from districts_forsearch
-            order by levenshtein(unaccent(lower(district)), unaccent(lower($1)))
-            limit 1),
-        specific_points as (select * from planet_osm_point, specific_district
-        where boundary isnull and (name notnull or "addr:street" notnull or "addr:housenumber" notnull) and st_contains(specific_district.district_geometry, way))
+        select district, district_geometry from districts_forsearch
+        order by levenshtein(unaccent(lower(district)), unaccent(lower($1)))
+        limit 1),
+        specific_ward as (select ward, ward_geometry from wards_forsearch, specific_district
+        where st_contains(specific_district.district_geometry, ward_geometry)
+        order by levenshtein(unaccent(lower(ward)), unaccent(lower($2)))
+        limit 1),
+        specific_roads as (select * from planet_osm_line,specific_ward
+        where boundary isnull and name notnull and st_contains(specific_ward.ward_geometry, way)),
+        specific_points as (select * from planet_osm_point,specific_ward
+        where boundary isnull and (name notnull or "addr:street" notnull or "addr:housenumber" notnull) and st_contains(specific_ward.ward_geometry, way))
 
         select osm_id::text, "addr:housenumber", "addr:street", name from specific_points
-`, district, ward)
+        `, district, ward)
+        /////////////////////////////////////////////////// new version
+//         let points = await prisma.$queryRawUnsafe(`WITH
+//         specific_district as (
+//             select district, district_geometry from districts_forsearch
+//             order by levenshtein(unaccent(lower(district)), unaccent(lower($1)))
+//             limit 1),
+//         specific_points as (select * from planet_osm_point, specific_district
+//         where boundary isnull and (name notnull or "addr:street" notnull or "addr:housenumber" notnull) and st_contains(specific_district.district_geometry, way))
+
+//         select osm_id::text, "addr:housenumber", "addr:street", name from specific_points
+// `, district, ward)
 
         // loop points and find the nearest street also fulfil null street by nearest street
         points = points.map(async (point: any) => {
@@ -145,33 +145,33 @@ export default async function handler(req: CustomNextApiRequest, res: NextApiRes
         })
         /////////////////////////////////////////////////////////////////////////////////////////////////
         ///////////////////////////// old version
-        //     let polygons = await prisma.$queryRawUnsafe(`WITH
-        // specific_district as (
-        // select district, district_geometry from districts_forsearch
-        // order by levenshtein(unaccent(lower(district)), unaccent(lower($1)))
-        // limit 1),
-        // specific_ward as (select ward, ward_geometry from wards_forsearch, specific_district
-        // where st_contains(specific_district.district_geometry, ward_geometry)
-        // order by levenshtein(unaccent(lower(ward)), unaccent(lower($2)))
-        // limit 1),
-        // specific_roads as (select * from planet_osm_line,specific_ward
-        // where boundary isnull and name notnull and st_contains(specific_ward.ward_geometry, way)),
-        // specific_polygons as (select * from planet_osm_polygon,specific_ward
-        // where boundary isnull and (name notnull or "addr:street" notnull or "addr:housenumber" notnull) and st_contains(specific_ward.ward_geometry, way))
-
-        // select osm_id::text, "addr:housenumber", "addr:street", name from specific_polygons
-        // `, district, ward)
-        /////////////////////////////////////////////////// new version
-        let polygons = await prisma.$queryRawUnsafe(`WITH
+            let polygons = await prisma.$queryRawUnsafe(`WITH
         specific_district as (
         select district, district_geometry from districts_forsearch
         order by levenshtein(unaccent(lower(district)), unaccent(lower($1)))
         limit 1),
-        specific_polygons as (select * from planet_osm_polygon,specific_district
-        where boundary isnull and (name notnull or "addr:street" notnull or "addr:housenumber" notnull) and st_contains(specific_district.district_geometry, way))
-    
+        specific_ward as (select ward, ward_geometry from wards_forsearch, specific_district
+        where st_contains(specific_district.district_geometry, ward_geometry)
+        order by levenshtein(unaccent(lower(ward)), unaccent(lower($2)))
+        limit 1),
+        specific_roads as (select * from planet_osm_line,specific_ward
+        where boundary isnull and name notnull and st_contains(specific_ward.ward_geometry, way)),
+        specific_polygons as (select * from planet_osm_polygon,specific_ward
+        where boundary isnull and (name notnull or "addr:street" notnull or "addr:housenumber" notnull) and st_contains(specific_ward.ward_geometry, way))
+
         select osm_id::text, "addr:housenumber", "addr:street", name from specific_polygons
         `, district, ward)
+        /////////////////////////////////////////////////// new version
+        // let polygons = await prisma.$queryRawUnsafe(`WITH
+        // specific_district as (
+        // select district, district_geometry from districts_forsearch
+        // order by levenshtein(unaccent(lower(district)), unaccent(lower($1)))
+        // limit 1),
+        // specific_polygons as (select * from planet_osm_polygon,specific_district
+        // where boundary isnull and (name notnull or "addr:street" notnull or "addr:housenumber" notnull) and st_contains(specific_district.district_geometry, way))
+    
+        // select osm_id::text, "addr:housenumber", "addr:street", name from specific_polygons
+        // `, district, ward)
 
         // // loop polygons and find the nearest street also fulfil null street by nearest street
         polygons = polygons.map(async (polygon: any) => {
@@ -209,147 +209,146 @@ export default async function handler(req: CustomNextApiRequest, res: NextApiRes
         })
 
         ////////////////////////////////////////////// version 10 stuff
-        res.status(200).json(points)
-        // let result = []
-        // // get 10 highest totalDistance from both merged array of points polygons
-        // let pointIndex = 0
-        // let polygonIndex = 0
-        // // until result length = 10 not stop
-        // while (result.length < 10 && pointIndex < points.length && polygonIndex < polygons.length) {
-        //     // if both points and polygons are not empty
-        //     if (pointIndex < points.length && polygonIndex < polygons.length) {
-        //         // compare totalDistance
-        //         if (points[pointIndex].totalDistance < polygons[polygonIndex].totalDistance) {
-        //             // if points totalDistance is smaller, push it to result
-        //             // add typeOfShape
-        //             points[pointIndex].typeOfShape = 'point'
-        //             result.push(points[pointIndex])
-        //             pointIndex++
-        //         }
-        //         else {
-        //             // if polygons totalDistance is smaller, push it to result
-        //             // add typeOfShape
-        //             polygons[polygonIndex].typeOfShape = 'polygon'
-        //             result.push(polygons[polygonIndex])
-        //             polygonIndex++
-        //         }
-        //     }
-        //     // if points is empty
-        //     else if (pointIndex >= points.length) {
-        //         // push polygons to result
-        //         polygons[polygonIndex].typeOfShape = 'polygon'
-        //         result.push(polygons[polygonIndex])
-        //         polygonIndex++
-        //     }
-        //     // if polygons is empty
-        //     else if (polygonIndex >= polygons.length) {
-        //         // push points to result
-        //         points[pointIndex].typeOfShape = 'point'
-        //         result.push(points[pointIndex])
-        //         pointIndex++
-        //     }
-        // }
-        // // loop result and get lat lng
-        // result = result.map(async (element: any) => {
-        //     let forResult
-        //     if (element.typeOfShape === 'point') {
-        //         // turn osm_id to bigInit
-        //         let id = BigInt(element.osm_id)
-        //         forResult = await prisma.$queryRawUnsafe(`select 
-        //         amenity, shop, tourism, historic,
-        //         st_y(st_transform(way,4326)) as lat, 
-        //         st_x(st_transform(way,4326)) as lng from planet_osm_point where osm_id = $1`, id)
-        //         // for type
-        //         let type = ""
-        //         if (forResult[0].amenity) {
-        //             type = forResult[0].amenity
-        //         }
-        //         else if (forResult[0].shop) {
-        //             type = forResult[0].shop
-        //         }
-        //         else if (forResult[0].tourism) {
-        //             type = forResult[0].tourism
-        //         }
-        //         else if (forResult[0].historic) {
-        //             type = forResult[0].historic
-        //         }
-        //         else if (type === "" && element["addr:housenumber"]) {
-        //             type = "house"
-        //         }
-        //         else if (type === "") {
-        //             type = "unknown"
-        //         }
-        //         element.type = type
-        //     }
-        //     else if (element.typeOfShape === 'polygon') {
-        //         // turn osm_id to bigInit
-        //         let id = BigInt(element.osm_id)
-        //         forResult = await prisma.$queryRawUnsafe(`select 
-        //         landuse, building, amenity, leisure, shop,
-        //         st_y(st_transform(st_centroid(way),4326)) as lat, 
-        //         st_x(st_transform(st_centroid(way),4326)) as lng from planet_osm_polygon where osm_id = $1`, id)
-        //         // for type
-        //         let type = ""
-        //         if (forResult[0].building && forResult[0].building !== "yes") {
-        //             type = forResult[0].building
-        //         }
-        //         else if (forResult[0].amenity) {
-        //             type = forResult[0].amenity
-        //         }
-        //         else if (forResult[0].landuse) {
-        //             type = forResult[0].landuse
-        //         }
-        //         else if (forResult[0].leisure) {
-        //             type = forResult[0].leisure
-        //         }
-        //         else if (forResult[0].shop) {
-        //             type = forResult[0].shop
-        //         }
-        //         else if (type === "") {
-        //             type = "unknown"
-        //         }
-        //         element.type = type
-        //     }
-        //     element.lat = forResult[0].lat
-        //     element.lng = forResult[0].lng
-        //     let fullAddress = ''
-        //     if (element["name"]) {
-        //         fullAddress += element["name"]
-        //     }
-        //     if (element["addr:housenumber"]) {
-        //         fullAddress += ' ' + element["addr:housenumber"]
-        //     }
-        //     if (element["addr:street"]) {
-        //         fullAddress += ' ' + element["addr:street"]
-        //     }
-        //     // add ward and district
-        //     if (ward) {
-        //         fullAddress += ', ' + ward
-        //     }
-        //     if (district) {
-        //         fullAddress += ', ' + district
-        //     }
-        //     // add city
-        //     fullAddress += ', ' + "Thành phố Hồ Chí Minh"
-        //     element.fullAddress = fullAddress
+        let result = []
+        // get 10 highest totalDistance from both merged array of points polygons
+        let pointIndex = 0
+        let polygonIndex = 0
+        // until result length = 10 not stop
+        while (result.length < 10 && pointIndex < points.length && polygonIndex < polygons.length) {
+            // if both points and polygons are not empty
+            if (pointIndex < points.length && polygonIndex < polygons.length) {
+                // compare totalDistance
+                if (points[pointIndex].totalDistance < polygons[polygonIndex].totalDistance) {
+                    // if points totalDistance is smaller, push it to result
+                    // add typeOfShape
+                    points[pointIndex].typeOfShape = 'point'
+                    result.push(points[pointIndex])
+                    pointIndex++
+                }
+                else {
+                    // if polygons totalDistance is smaller, push it to result
+                    // add typeOfShape
+                    polygons[polygonIndex].typeOfShape = 'polygon'
+                    result.push(polygons[polygonIndex])
+                    polygonIndex++
+                }
+            }
+            // if points is empty
+            else if (pointIndex >= points.length) {
+                // push polygons to result
+                polygons[polygonIndex].typeOfShape = 'polygon'
+                result.push(polygons[polygonIndex])
+                polygonIndex++
+            }
+            // if polygons is empty
+            else if (polygonIndex >= polygons.length) {
+                // push points to result
+                points[pointIndex].typeOfShape = 'point'
+                result.push(points[pointIndex])
+                pointIndex++
+            }
+        }
+        // loop result and get lat lng
+        result = result.map(async (element: any) => {
+            let forResult
+            if (element.typeOfShape === 'point') {
+                // turn osm_id to bigInit
+                let id = BigInt(element.osm_id)
+                forResult = await prisma.$queryRawUnsafe(`select 
+                amenity, shop, tourism, historic,
+                st_y(st_transform(way,4326)) as lat, 
+                st_x(st_transform(way,4326)) as lng from planet_osm_point where osm_id = $1`, id)
+                // for type
+                let type = ""
+                if (forResult[0].amenity) {
+                    type = forResult[0].amenity
+                }
+                else if (forResult[0].shop) {
+                    type = forResult[0].shop
+                }
+                else if (forResult[0].tourism) {
+                    type = forResult[0].tourism
+                }
+                else if (forResult[0].historic) {
+                    type = forResult[0].historic
+                }
+                else if (type === "" && element["addr:housenumber"]) {
+                    type = "house"
+                }
+                else if (type === "") {
+                    type = "unknown"
+                }
+                element.type = type
+            }
+            else if (element.typeOfShape === 'polygon') {
+                // turn osm_id to bigInit
+                let id = BigInt(element.osm_id)
+                forResult = await prisma.$queryRawUnsafe(`select 
+                landuse, building, amenity, leisure, shop,
+                st_y(st_transform(st_centroid(way),4326)) as lat, 
+                st_x(st_transform(st_centroid(way),4326)) as lng from planet_osm_polygon where osm_id = $1`, id)
+                // for type
+                let type = ""
+                if (forResult[0].building && forResult[0].building !== "yes") {
+                    type = forResult[0].building
+                }
+                else if (forResult[0].amenity) {
+                    type = forResult[0].amenity
+                }
+                else if (forResult[0].landuse) {
+                    type = forResult[0].landuse
+                }
+                else if (forResult[0].leisure) {
+                    type = forResult[0].leisure
+                }
+                else if (forResult[0].shop) {
+                    type = forResult[0].shop
+                }
+                else if (type === "") {
+                    type = "unknown"
+                }
+                element.type = type
+            }
+            element.lat = forResult[0].lat
+            element.lng = forResult[0].lng
+            let fullAddress = ''
+            if (element["name"]) {
+                fullAddress += element["name"]
+            }
+            if (element["addr:housenumber"]) {
+                fullAddress += ' ' + element["addr:housenumber"]
+            }
+            if (element["addr:street"]) {
+                fullAddress += ' ' + element["addr:street"]
+            }
+            // add ward and district
+            if (ward) {
+                fullAddress += ', ' + ward
+            }
+            if (district) {
+                fullAddress += ', ' + district
+            }
+            // add city
+            fullAddress += ', ' + "Thành phố Hồ Chí Minh"
+            element.fullAddress = fullAddress
 
-        //     return {
-        //         osm_id: element.osm_id,
-        //         address: element.fullAddress,
-        //         type: element.type,
-        //         typeOfShape: element.typeOfShape,
-        //         lat: element.lat,
-        //         lng: element.lng
-        //     }
-        // }
-        // )
-        // // wait for all promises to resolve
-        // result = await Promise.all(result)
-        // res.status(200).json({
-        //     state: "success",
-        //     message: "Your request is successful",
-        //     data: result
-        // })
+            return {
+                osm_id: element.osm_id,
+                address: element.fullAddress,
+                type: element.type,
+                typeOfShape: element.typeOfShape,
+                lat: element.lat,
+                lng: element.lng
+            }
+        }
+        )
+        // wait for all promises to resolve
+        result = await Promise.all(result)
+        res.status(200).json({
+            state: "success",
+            message: "Your request is successful",
+            data: result
+        })
     }
     else {
         res.status(400).json({
