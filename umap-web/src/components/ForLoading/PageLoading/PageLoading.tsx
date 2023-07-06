0c31045a81bad974816964c2cdc16e3560a5047a
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-07-06 13:38:25 +0700

    change to fetch again, finish session

--- umap-web/src/components/ForLoading/PageLoading/PageLoading.tsx
+++ umap-web/src/components/ForLoading/PageLoading/PageLoading.tsx
@@ -3,7 +3,7 @@ import Loading from "../Loading/Loading";
 
 function PageLoading() {
     return ( <>
-        <div className="w-full h-screen flex justify-center items-center bg-pink-300">
+        <div className="w-full h-screen flex justify-center items-center bg-white">
             <Loading/>
         </div>
     </> );
--- umap-web/src/components/Map/Map.tsx
+++ umap-web/src/components/Map/Map.tsx
@@ -1,55 +1,67 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Event from "../MapTools/Event/Event";
 import LocationMarkers from "../MapTools/LocationMarker/LocationMarker";
 import { MapContainer, ZoomControl, WMSTileLayer, LayersControl } from "react-leaflet";
 import './Map.css';
-import useSWR from 'swr'
 import PageLoading from "../ForLoading/PageLoading/PageLoading";
 
 const { BaseLayer } = LayersControl;
 export default function MapView() {
   const mapRef = useRef<any>(null)
-  // use swr
-  const fetcher = (url: string) => fetch(url, { method: 'GET' })
-    .then((res) => res.json())
-    .then(result => result)
-  const { data, error, isLoading } = useSWR('http://localhost:3000/api/session/', fetcher)
-  const center = data?.center
-  const zoom = data?.zoom
-  console.log(data, isLoading)
+  const [view, setView] = useState<any>(false)
+
+  useEffect(()=>{
+    const fetchData = async () => {
+    const response = await fetch("http://localhost:3000/api/session/", {method: 'GET'})
+    .then(response=>response.json())
+    .then(result=>result)
+    console.log(response)
+    if(response.zoom===null||response.center===null){
+      setView({
+        center: {lat:10.879961,lng:106.810877},
+        zoom: 12
+      })
+    }else{
+      setView({
+        center: response.center,
+        zoom: response.zoom
+      })
+    }
+  }
+    fetchData()
+  }, [])
 
   return (
     <>
       {
-        // isLoading?
+        !view?
           <PageLoading/>
-        // :
-        // // data&&
-        // // <MapContainer
-        // //   // @ts-ignore
-        // //   center={center}
-        // //   // @ts-ignore
-        // //   zoom={zoom}
-        // //   scrollWheelZoom={true}
-        // //   zoomControl={false}
-        // //   style={{ height: "100vh", width: "100vw" }}
-        // //   ref={mapRef}
-        // // >
+        :
+        <MapContainer
+          // @ts-ignore
+          center={view.center}
+          // @ts-ignore
+          zoom={view.zoom}
+          scrollWheelZoom={true}
+          zoomControl={false}
+          style={{ height: "100vh", width: "100vw" }}
+          ref={mapRef}
+        >
 
-        // //   <LayersControl>
-        // //     <BaseLayer checked name="U-MAP">
-        // //       <WMSTileLayer url="https://umap.dientoan.vn/geoserver/ows?" layers='TVtesting:planet_osm_line' />
-        // //     </BaseLayer>
-        // //     <BaseLayer checked name="OSM">
-        // //       <WMSTileLayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
-        // //     </BaseLayer>
-        // //   </LayersControl>
+          <LayersControl>
+            <BaseLayer checked name="U-MAP">
+              <WMSTileLayer url="https://umap.dientoan.vn/geoserver/ows?" layers='TVtesting:planet_osm_line' />
+            </BaseLayer>
+            <BaseLayer checked name="OSM">
+              <WMSTileLayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
+            </BaseLayer>
+          </LayersControl>
 
-        // //   <ZoomControl position="topright" />
-        // //   <LocationMarkers />
-        // //   <Event />
-        // // </MapContainer>
+          <ZoomControl position="topright" />
+          <LocationMarkers />
+          <Event />
+        </MapContainer>
       }
     </>
   );
