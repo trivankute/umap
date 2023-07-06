04b6d1abcec051531870bfea6aed5ec11355e249
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-07-06 13:03:40 +0700

    add time before session save

--- umap-web/src/pages/api/session/index.ts
+++ umap-web/src/pages/api/session/index.ts
@@ -1,25 +1,24 @@
 import { getSession } from "@/services/getSession";
 export const config = {
-    api: {
-      externalResolver: true,
-    },
+  api: {
+    externalResolver: true,
+  },
 };
 
-export default async function handler(req:any, res:any) {
-    const session = await getSession(req, res);
-    console.log(session)
-    if(req.method==='GET'){
-        res.status(200).json({
-            'center': session.center??null,
-            'zoom': session.zoom??null
-        })
-    }
+export default async function handler(req: any, res: any) {
+  const session = await getSession(req, res);
+  if (req.method === 'GET') {
+    res.status(200).json({
+      'center': session.center ?? null,
+      'zoom': session.zoom ?? null
+    })
+  }
 
-    if(req.method==='POST'){
+  if (req.method === 'POST') {
     const data = JSON.parse(req.body)
     session.center = data.center
     session.zoom = data.zoom
-    
+
     res.status(200).json({ saved: "Data saved in session" });
     return
   }
