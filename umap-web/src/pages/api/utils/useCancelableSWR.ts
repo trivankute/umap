e1552967fcc113fe1f083c0c8f126bca1a624e80
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-07-19 22:42:51 +0700

    commit temporary for cancel serverprocessing

--- umap-web/src/pages/api/utils/useCancelableSWR.ts
+++ umap-web/src/pages/api/utils/useCancelableSWR.ts
@@ -3,7 +3,7 @@ import useSWR from "swr"
 export default function useCancelableSWR (key:any, opts:any) {
     const controller = new AbortController()
     const fetcher = async (...args:[any]) => {
-        return fetch(...args).then(res => res.json())
+        return fetch(...args).then(res => res.json()).catch(err => {console.log(err)})
     };
     // @ts-ignore
     return [useSWR(key, url=> fetcher(url, { signal: controller.signal }), opts), controller]
