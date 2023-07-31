e727470fffe3028106fb2a392de92a5455147f64
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-07-31 17:23:07 +0700

    lack of way::text make search full address not work

--- umap-web/src/pages/api/utils/findWard.ts
+++ umap-web/src/pages/api/utils/findWard.ts
@@ -3,7 +3,7 @@ export default async function handler(prisma: any, ward: string, district: strin
     if(!district && !city) 
     {
         result = await prisma.$queryRawUnsafe(`
-        select ward, district, city, st_asgeojson(ST_Boundary(st_transform(ward_way,4326))), st_x(st_transform(st_centroid(ward_way),4326)), st_y(st_transform(st_centroid(ward_way),4326)) from wards_forsearch
+        select ward, district, city, ward_way::text, st_asgeojson(ST_Boundary(st_transform(ward_way,4326))), st_x(st_transform(st_centroid(ward_way),4326)), st_y(st_transform(st_centroid(ward_way),4326)) from wards_forsearch
         where lower(unaccent(ward)) = lower(unaccent($1))
         limit 10
         `, ward)
@@ -11,7 +11,7 @@ export default async function handler(prisma: any, ward: string, district: strin
     }
     else {
         result = await prisma.$queryRawUnsafe(`
-        select ward, district, city, st_asgeojson(ST_Boundary(st_transform(ward_way,4326))), st_x(st_transform(st_centroid(ward_way),4326)), st_y(st_transform(st_centroid(ward_way),4326)) from wards_forsearch
+        select ward, district, city, ward_way::text, st_asgeojson(ST_Boundary(st_transform(ward_way,4326))), st_x(st_transform(st_centroid(ward_way),4326)), st_y(st_transform(st_centroid(ward_way),4326)) from wards_forsearch
         where lower(unaccent(ward)) = lower(unaccent($1))
         ${district !== false ? 'and lower(unaccent(district)) = lower(unaccent($2))' : ''}
         ${city !== false ? 'and lower(unaccent(city)) = lower(unaccent($3))' : ''}   
