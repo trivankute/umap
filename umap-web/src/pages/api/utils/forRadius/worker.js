7633100b7c3f52989e122f711fff313091126491
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2023-07-31 17:23:52 +0700

    add check global prisma for instances problem

--- umap-web/src/pages/api/utils/forRadius/worker.js
+++ umap-web/src/pages/api/utils/forRadius/worker.js
@@ -1,6 +1,21 @@
 const { PrismaClient } = require('@prisma/client');
 const { parentPort } = require('worker_threads');
-const prisma = new PrismaClient()
+let prisma;
+
+if (typeof window === "undefined") {
+    if (process.env.NODE_ENV === "production") {
+        prisma = new PrismaClient();
+    } else {
+        // @ts-ignore
+        if (!global.prisma) {
+        // @ts-ignore
+            global.prisma = new PrismaClient();
+        }
+
+        // @ts-ignore
+        prisma = global.prisma;
+    }
+}
 const nearestAddress = require('../nearestAddressFromCoor.js')
 
 async function forRadiusWorker( lng, lat) {
